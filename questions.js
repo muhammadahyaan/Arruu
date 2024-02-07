8dfa1cf11c4d3c5eccf95055e86db7ee5ac1112d
muhammadahyaan <147310460+muhammadahyaan@users.noreply.github.com>
2024-02-07 14:48:16 +0500

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,36 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+      numb: 1,
+      question: "Where do you reside in my world?",
+      answer: "  My Heart 💘",
+      options: [
+        "  My Heart 💘",
+        " You dont",
+        "  ",
+        "delulu"
+      ]
+    },
+    {
+      numb: 2,
+      question: "Do you hold some importance in my world?",
+      answer:"you are my world",
+      options: [
+        "YES, but not that much",
+        "You dont",
+        "A LOT",
+        "you are my world"
+      ]
+    },
+    {
+      numb: 3,
+      question: "is our love unconditional?",
+      answer: "Yes ,our love is eternal and unconditional",
+      options: [
+        "NO",
+        "yes",
+        "Yes ,our love is eternal and unconditional"
+        
+      ]
+    },
+];
