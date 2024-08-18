44d4c1800632d37cff8444091128420b4b74ae60
muhammadahyaan <147310460+muhammadahyaan@users.noreply.github.com>
2024-08-18 11:06:28 -0700

    Update script.js

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ const timeText = document.querySelector(".timer .time_left_txt");
 const timeCount = document.querySelector(".timer .timer_sec");
 
 // if startQuiz button clicked
-start_btn.onclick() = ()=>{
+start_btn.onclick =Onclick (){
     info_box.classList.add("activeInfo"); //show info box
 }
 
