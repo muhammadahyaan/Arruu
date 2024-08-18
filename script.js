3974045f88b1d900404f600bc807bced719be9f7
muhammadahyaan <147310460+muhammadahyaan@users.noreply.github.com>
2024-08-18 11:08:08 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 //selecting all required elements
-const start_btn = document.querySelector(".start_btn button");
+var start_btn = document.querySelector(".start_btn button");
 const info_box = document.querySelector(".info_box");
 const exit_btn = document.querySelector(".buttons .quit");
 const continue_btn = document.querySelector(".buttons .restart");
